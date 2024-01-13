e7fdeae4a06f3f03aa7d19f42a09eb9941186062
Apolonia Abramowicz <pola.abramowicz@onet.pl>
2024-01-13 04:14:49 +0100

    "UI ready"

--- src/Scene.java
+++ src/Scene.java
@@ -7,13 +7,19 @@ import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 
 public class Scene {
+    //Reading the source image in to a Mat object
+    private Mat src;
 
     private LinkedList<Item> items;
+    private String name;
 //    private Segment segment;
 //    private Segment segment1;
 
     public Scene() {
         items= new LinkedList<>();
+        System.loadLibrary( Core.NATIVE_LIBRARY_NAME );
+        src = Imgcodecs.imread("C:\\Users\\Apolonia\\Pictures\\white.jpg");
+        name = "drawing a scene";
 //        segment= new Segment(new Point(100,300),new Point(100,400));
 //        segment1= new Segment(new Point(300,800),new Point(30,400));
     }
@@ -26,20 +32,21 @@ public class Scene {
         //Loading the OpenCV core library
         //System.loadLibrary( Core.NATIVE_LIBRARY_NAME );
         //Loading the OpenCV core library
-        if(items.size()==0) {
-            System.out.println("Pusta scena");
-            return;
-        }
-        System.loadLibrary( Core.NATIVE_LIBRARY_NAME );
-        //Reading the source image in to a Mat object
-        Mat src = Imgcodecs.imread("C:\\Users\\Apolonia\\Pictures\\white.jpg");
+//        if(items.size()==0) {
+//            System.out.println("Pusta scena");
+//            return;
+//        }
+//        System.loadLibrary( Core.NATIVE_LIBRARY_NAME );
+//        //Reading the source image in to a Mat object
+//        Mat src = Imgcodecs.imread("C:\\Users\\Apolonia\\Pictures\\white.jpg");
         for (Item i: items) {
             i.draw(src);
         }
 //        segment.draw(src);
 //        segment1.draw(src);
-        HighGui.imshow("Drawing a scene", src);
+        HighGui.imshow(name, src);
         HighGui.waitKey();
+        name = name+ "1";
 
 
     }
